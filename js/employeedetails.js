d6efe8390dc9ff6bc7283e94b35e2cf42d8bb457
romaingorce <rom.gorce@gmail.com>
2015-04-08 16:49:27 +0200

    Update employeedetails.js

--- js/employeedetails.js
+++ js/employeedetails.js
@@ -1,4 +1,3 @@
-localStorage['serviceURL'] = "http://demo.batiactu.info/WebServiceRomain/";
 var serviceURL = localStorage['serviceURL'];
 var scroll = new iScroll('wrapper', { vScrollbar: false, hScrollbar:false, hScroll: false });
 
