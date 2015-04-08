1664ea03b825be76d3558c98f73d817b01910139
romaingorce <rom.gorce@gmail.com>
2015-04-08 16:57:39 +0200

    Update employeelist.js

--- js/employeelist.js
+++ js/employeelist.js
@@ -1,5 +1,4 @@
-localStorage['serviceURL'] = "http://demo.batiactu.info/WebServiceRomain/";
-var serviceURL = localStorage['serviceURL'];
+var serviceURL = 'http://demo.batiactu.info/WebServiceRomain/';
 
 var scroll = new iScroll('wrapper', { vScrollbar: false, hScrollbar:false, hScroll: false });
 
@@ -16,7 +15,7 @@ $(document).ajaxError(function(event, request, settings) {
 
 function getEmployeeList() {
 	$('#busy').show();
-	$.getJSON(serviceURL + 'demo.batiactu.info/WebServiceRomain/getemployees.php', function(data) {
+	$.getJSON(serviceURL + 'getemployees.php', function(data) {
 		$('#busy').hide();
 		$('#employeeList li').remove();
 		employees = data.items;
