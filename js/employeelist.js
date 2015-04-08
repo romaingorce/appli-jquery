76417a8b89db1c056b62fd096d99cb46824716b5
romaingorce <rom.gorce@gmail.com>
2015-04-08 17:13:05 +0200

    Update employeelist.js

--- js/employeelist.js
+++ js/employeelist.js
@@ -15,6 +15,7 @@ $(document).ajaxError(function(event, request, settings) {
 
 function getEmployeeList() {
 	$('#busy').show();
+	alert(serviceURL + 'getemployees.php');
 	$.getJSON(serviceURL + 'getemployees.php', function(data) {
 		$('#busy').hide();
 		$('#employeeList li').remove();
